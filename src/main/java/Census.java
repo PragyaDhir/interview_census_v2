c5fc251930fce58384f328aca7bbbfb43274837f
Pragya <pragya.dhir97@gmail.com>
2025-02-06 12:25:10 +1100

    handle multiple regions todo :- 1. handle ties 2. handle multithreaded approach

--- src/main/java/Census.java
+++ src/main/java/Census.java
@@ -71,17 +71,6 @@ public class Census {
             // region not found
 
         }
-
-
-
-//        In the example below, the top three are ages 10, 15 and 12
-//        return new String[]{
-//                String.format(OUTPUT_FORMAT, 1, 10, 38),
-//                String.format(OUTPUT_FORMAT, 2, 15, 35),
-//                String.format(OUTPUT_FORMAT, 3, 12, 30)
-//        };
-
-        //throw new UnsupportedOperationException();
     }
 
     /**
@@ -90,21 +79,40 @@ public class Census {
      * We expect you to make use of all cores in the machine, specified by {@link #CORES).
      */
     public String[] top3Ages(List<String> regionNames) {
-        String [] top3Ages = new String[3];
+        List<String> top3Ages = new ArrayList<>();
+        HashMap<Integer,Integer> ageCountMap = new HashMap<>();
 
-        for(String region :regionNames)
-        {
-            top3Ages(region);
+        for(String region :regionNames) {
+            try (AgeInputIterator ageInputIterator = iteratorFactory.apply(region)) {
+                while (ageInputIterator.hasNext()) {
+                    int age = ageInputIterator.next();
+                    if (age >= 0) {
+                        ageCountMap.put(age, ageCountMap.getOrDefault(age, 0) + 1);
+                    } else throw new IllegalArgumentException("Age is invalid");
+
+                }
+
+
+            } catch (IOException ioException) {
+                throw new RuntimeException("Error occurred while closing iterator");
+            } catch (RuntimeException re) {
+                if (re instanceof IllegalArgumentException)
+                    throw new RuntimeException(re.getMessage());
+                // region not found
+
+            }
         }
 
-//        In the example below, the top three are ages 10, 15 and 12
-//        return new String[]{
-//                String.format(OUTPUT_FORMAT, 1, 10, 38),
-//                String.format(OUTPUT_FORMAT, 2, 15, 35),
-//                String.format(OUTPUT_FORMAT, 3, 12, 30)
-//        };
 
-        return  top3Ages;
+        List<Integer> keys = ageCountMap.entrySet().stream().sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed()).limit(3).map(Map.Entry::getKey).collect(Collectors.toList());
+
+        for (int i = 0; i < keys.size(); i++) {
+            top3Ages.add(String.format(OUTPUT_FORMAT, i + 1, keys.get(i), ageCountMap.get(keys.get(i))));
+
+        }
+
+        return top3Ages.toArray(new String[0]);
+
     }
 
 
